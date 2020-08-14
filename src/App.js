6fd7d37fc9c535eb5b3fb6ea1c015e24c7ca831e
Domirop <domi.trivi2000@hotmail.com>
2020-08-14 22:29:10 +0100

    Feature: Added intl library for internationalization app

--- src/App.js
+++ src/App.js
@@ -1,26 +1,41 @@
-import React from 'react';
-import logo from './logo.svg';
+import React from "react";
+import {FormattedMessage, IntlProvider} from 'react-intl';
 import './App.css';
+import locale_en from "./translations/en.json"
+import locale_fr from "./translations/fr.json";
+import locale_al from "./translations/al.json";
+import locale_it from "./translations/it.json";
+import locale_sp from "./translations/sp.json";
+
+const data = {
+    'sp': locale_sp,
+    'en': locale_en,
+    'fr': locale_fr,
+    'al': locale_al,
+    'it': locale_it,
+};
+
+const language = "sp";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <IntlProvider locale={language} messages={data[language]}>
+            <div className="App">
+                <p>
+                    <FormattedMessage
+                        id="combo.2"
+                        defaultMessage="Hello, welcome"
+                        description="Welcome message"
+                        values={{ user: "John Doe", pepe: "Antonio" }}/>
+                        <h1></h1>
+                    <FormattedMessage
+                        id="combo.price.2"
+                        defaultMessage="No tiene valor."
+                        description="Welcome message"/>
+                </p>
+            </div>
+        </IntlProvider>
+    );
 }
 
 export default App;
