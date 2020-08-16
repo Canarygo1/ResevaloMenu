8299c298909a7c125feda038835c6ec5ea1c7983
Alejandro <alejandro.cruz@atani.com>
2020-08-16 18:18:11 +0100

    Refactor: new Router and boostrap was added

--- src/App.js
+++ src/App.js
@@ -6,6 +6,9 @@ import locale_fr from "./translations/fr.json";
 import locale_al from "./translations/al.json";
 import locale_it from "./translations/it.json";
 import locale_sp from "./translations/sp.json";
+import { createBrowserHistory } from "history";
+import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
+const history = createBrowserHistory();
 
 const data = {
     'sp': locale_sp,
@@ -19,22 +22,29 @@ const language = "sp";
 
 function App() {
     return (
-        <IntlProvider locale={language} messages={data[language]}>
-            <div className="App">
-                <p>
-                    <FormattedMessage
-                        id="combo.2"
-                        defaultMessage="Hello, welcome"
-                        description="Welcome message"
-                        values={{ user: "John Doe", pepe: "Antonio" }}/>
-                        <h1></h1>
-                    <FormattedMessage
-                        id="combo.price.2"
-                        defaultMessage="No tiene valor."
-                        description="Welcome message"/>
-                </p>
-            </div>
-        </IntlProvider>
+
+      <Router history={history}>
+        <Switch>
+          <Route path="/cartas/:id" exact>
+             <IntlProvider locale={language} messages={data[language]}>
+
+                <div className="App">
+                    <p>
+                       <FormattedMessage
+                           id="combo.2"
+                          defaultMessage="Hello, welcome"
+                          description="Welcome message"
+                         values={{ user: "John Doe", pepe: "Antonio" }}/>
+                         <FormattedMessage
+                             id="combo.price.2"
+                             defaultMessage="No tiene valor."
+                             description="Welcome message"/>
+                     </p>
+                </div>
+             </IntlProvider>
+          </Route>
+        </Switch>
+      </Router>
     );
 }
 
