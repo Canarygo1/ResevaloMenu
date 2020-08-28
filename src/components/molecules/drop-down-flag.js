d43993a473997e1efd6d9c28939b49b07f4a7c56
Domirop <domi.trivi2000@hotmail.com>
2020-08-28 13:29:41 +0100

    release: add new flag library version: 1.0.3

--- src/components/molecules/drop-down-flag.js
+++ src/components/molecules/drop-down-flag.js
@@ -1,46 +1,42 @@
-import React, {useState} from 'react';
-import Flag from 'react-world-flags'
+import React from 'react';
 import Dropdown from "react-bootstrap/Dropdown";
 import PropTypes from "prop-types";
+import 'flag-icon-css/css/flag-icon.css';
 
-const flags = ["es","fr","gb","it","de"];
+//const flags = ["es","fr","gb","it","de"];
 
 function DropDownFlag({codeDefault, id}) {
 
-    let [lenguageDefault, setLenguageDefault] = useState(codeDefault);
     return (
         <div style={{alignSelf:"center"}}>
             <Dropdown className={"dropClass"}>
                 <Dropdown.Toggle id="toggle-drop" variant={"secondary"}>
-                    <Flag code={flags[lenguageDefault]}
-                          height="20"
-                          fallback={ <span>Unknown</span> }
-                          style={{marginRight:10}}/>
+                    <span className="flag-icon flag-icon-es"></span>
                 </Dropdown.Toggle>
               <Dropdown.Menu className={"menu-drop"}>
-                <Dropdown.Item id={"item-drop-lenguage"} eventKey="0" onSelect={(eventKey) => setLenguageDefault(lenguageDefault = eventKey)}>
+                <Dropdown.Item id={"item-drop-lenguage"}>
                   <div onClick={() => window.location.replace(`/cartas/${id}/0`)}>
-                    <Flag code={flags[0]} height={20} fallback={ <span>Unknown</span> }/>
+                      <span className="flag-icon flag-icon-es flag-drop"></span>
                   </div>
                 </Dropdown.Item>
-                <Dropdown.Item id={"item-drop-lenguage"} eventKey="1" onSelect={(eventKey) => setLenguageDefault(lenguageDefault = eventKey)}>
+                <Dropdown.Item id={"item-drop-lenguage"}>
                   <div onClick={() => window.location.replace(`/cartas/${id}/1`)}>
-                    <Flag code={flags[1]} height={20} fallback={ <span>Unknown</span> }/>
+                      <span className="flag-icon flag-icon-fr flag-drop"></span>
                   </div>
                 </Dropdown.Item>
-                <Dropdown.Item id={"item-drop-lenguage"} eventKey="2" onSelect={(eventKey) => setLenguageDefault(lenguageDefault = eventKey)}>
+                <Dropdown.Item id={"item-drop-lenguage"}>
                   <div onClick={() => window.location.replace(`/cartas/${id}/2`)}>
-                    <Flag code={flags[2]} height={20} style={{width:32}} fallback={ <span>Unknown</span> }/>
+                      <span className="flag-icon flag-icon-gb flag-drop"></span>
                   </div>
                 </Dropdown.Item>
-                <Dropdown.Item id={"item-drop-lenguage"} eventKey="3" onSelect={(eventKey) => setLenguageDefault(lenguageDefault = eventKey)}>
+                <Dropdown.Item id={"item-drop-lenguage"}>
                   <div onClick={() => window.location.replace(`/cartas/${id}/3`)}>
-                    <Flag code={flags[3]} height={20} fallback={ <span>Unknown</span> }/>
+                      <span className="flag-icon flag-icon-it flag-drop"></span>
                   </div>
                 </Dropdown.Item>
-                <Dropdown.Item id={"item-drop-lenguage"} eventKey="4" onSelect={(eventKey) => setLenguageDefault(lenguageDefault = eventKey)}>
+                <Dropdown.Item id={"item-drop-lenguage"}>
                   <div onClick={() => window.location.replace(`/cartas/${id}/4`)}>
-                    <Flag code={flags[4]} height={20} style={{width:32}}  fallback={ <span>Unknown</span> }/>
+                      <span className="flag-icon flag-icon-de flag-drop"></span>
                   </div>
                 </Dropdown.Item>
               </Dropdown.Menu>
--- src/pages/lenguage_selection.js
+++ src/pages/lenguage_selection.js
@@ -1,9 +1,9 @@
 import React, {Component} from "react";
 import Row from "react-bootstrap/Row";
-import Flag from "react-world-flags";
 import Col from "react-bootstrap/Col";
 import Image from "react-bootstrap/Image";
 import {Link} from "react-router-dom";
+import 'flag-icon-css/css/flag-icon.css';
 
 class LenguageSelection extends Component {
     render() {
@@ -21,19 +21,19 @@ class LenguageSelection extends Component {
                 <Col>
                     <Row className="justify-content-center" sm={1} lg={5} md={5} xl={5} xs={1}>
                         <Link to={`/cartas/${this.props.match.params.id}/0`} className="my-3">
-                            <Flag className="flags-styles col" code="es" height={100}/>
+                            <span className="flag-icon flag-icon-es"></span>
                         </Link>
                         <Link to={`/cartas/${this.props.match.params.id}/1`} className="my-3">
-                            <Flag className="flags-styles col" code="fr" height={100}/>
+                            <span className="flag-icon flag-icon-fr"></span>
                         </Link>
                         <Link to={`/cartas/${this.props.match.params.id}/2`} className="my-3">
-                            <Flag className="flags-styles col" code="gb" height={100}/>
+                            <span className="flag-icon flag-icon-gb"></span>
                         </Link>
                         <Link to={`/cartas/${this.props.match.params.id}/3`} className="my-3">
-                            <Flag className="flags-styles col" code="it" height={100}/>
+                            <span className="flag-icon flag-icon-it"></span>
                         </Link>
                         <Link to={`/cartas/${this.props.match.params.id}/4`} className="my-3">
-                            <Flag className="flags-styles col" code="de" height={100}/>
+                            <span className="flag-icon flag-icon-de"></span>
                         </Link>
                     </Row>
                 </Col>
