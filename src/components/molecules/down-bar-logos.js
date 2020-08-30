05439f373227e125b6a21d0f677bff3a19944f5d
Domirop <domi.trivi2000@hotmail.com>
2020-08-30 17:30:53 +0100

    release: down bar is not autodefined version: 1.0.6

--- src/components/molecules/down-bar-logos.js
+++ src/components/molecules/down-bar-logos.js
@@ -10,22 +10,28 @@ DownBarLogos.propTypes = {
     urls: PropTypes.array.isRequired
 };
 
+let posibilityIcons = [faFacebook, faInstagram, faGoogle]
+let posibilityUrls = ['facebook-url', 'instagram-url','web-url'];
+let posibilityClassNames = ['', 'instagram', 'google'];
+
+function checkUrls(element, props){
+    if(props.urls[element][posibilityUrls[element]].length !== 0){
+        return (
+            <FontAwesomeIcon icon={posibilityIcons[element]} className={`fa-2x mx-2 ${posibilityClassNames[element]}`}
+                             style={{color:"#3b5998"}}
+                             onClick={() =>{
+                                 window.open(props.urls[element][posibilityUrls[element]], '_blank');}
+                             }/>
+        );
+    }
+}
+
 function DownBarLogos(props) {
-    console.log(props.urls)
     return (
         <div className="modal-footer justify-content-center mx-4 mt-4">
-            <FontAwesomeIcon icon={faFacebook} className="fa-2x mx-2" style={{color:"#3b5998"}}
-                             onClick={() =>{
-                                 window.open(props.urls[0]["facebook-url"], '_blank');}
-                             }/>
-            <FontAwesomeIcon icon={faInstagram} className="fa-2x mx-2 instagram"
-                             onClick={() =>{
-                                 window.open(props.urls[1]["instagram-url"], '_blank');}
-                             }/>
-            <FontAwesomeIcon icon={faGoogle} className="fa-2x mx-2 google"
-                             onClick={() =>{
-                                 window.open(props.urls[2]["web-url"], '_blank');}
-                             }/>
+            {checkUrls(0, props)}
+            {checkUrls(1, props)}
+            {checkUrls(2, props)}
             <Image src={'/images/reservalo/logoReservaloCircle.png'} fluid={true}
                    className="logo-image-circle"
                    onClick={() =>{
