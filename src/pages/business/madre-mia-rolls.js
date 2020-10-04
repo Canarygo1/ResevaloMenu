1ba531074dd21176b836a35e8a5dfe27ba94c4a7
Domirop <domi.trivi2000@hotmail.com>
2020-10-04 17:35:23 +0100

    refactor: added lenguages

--- src/pages/business/madre-mia-rolls.js
+++ src/pages/business/madre-mia-rolls.js
@@ -1,5 +1,9 @@
 import React, {Component} from 'react';
 import locale_sp from "../../translations/madre-mia-rolls/sp.json";
+import locale_fr from "../../translations/madre-mia-rolls/fr.json";
+import locale_it from "../../translations/madre-mia-rolls/it.json";
+import locale_al from "../../translations/madre-mia-rolls/al.json";
+import locale_en from "../../translations/madre-mia-rolls/en.json";
 import {IntlProvider} from "react-intl";
 import Card from "../../components/organisms/card";
 import DownBarLogos from "../../components/molecules/down-bar-logos";
@@ -7,8 +11,8 @@ import dataBusiness from "../../business-data/madre-mia-rolls.json"
 import PropTypes from "prop-types";
 import Business from "../../business";
 
-const fileLenguages = [locale_sp];
-const codeLenguage = ["es"];
+const fileLenguages = [locale_sp, locale_fr, locale_en, locale_it, locale_al];
+const codeLenguage = ["es", "fr", "en", "it", "de"];
 
 const data = dataBusiness;
 
@@ -26,7 +30,7 @@ class MadreMiaRolls extends Component {
                 <IntlProvider locale={codeLenguage[codeDefault]} messages={fileLenguages[codeDefault]}>
                     <Card codeDefault={this.props.properties.match.params.codeDefault}
                           id={this.props.properties.match.params.id} dataBusiness={data}
-                          multiLenguage={false}/>
+                          multiLenguage={true}/>
                     <DownBarLogos urls={data["urls-website"]}/>
                 </IntlProvider>
             </div>
